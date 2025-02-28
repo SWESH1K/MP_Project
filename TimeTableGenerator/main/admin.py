26e4e3323fa4c9ecd7fc4ca8b3928fc8c5fe3f14
SWESH1K <vagalamsweshikreddy@gmail.com>
2025-02-28 19:20:13 +0530

    Models created

--- TimeTableGenerator/main/admin.py
+++ TimeTableGenerator/main/admin.py
@@ -1,3 +1,8 @@
 from django.contrib import admin
+from .models import Class, Subject, Faculty, Weekday
 
 # Register your models here.
+admin.site.register(Class)
+admin.site.register(Subject)
+admin.site.register(Faculty)
+admin.site.register(Weekday)
--- TimeTableGenerator/main/urls.py
+++ TimeTableGenerator/main/urls.py
@@ -3,6 +3,6 @@ from . import views
 
 urlpatterns = [
     path('', views.home, name='home'),
-    path('/generate', views.generate, name='generate'),
-    path('/generate_timetable', views.generate, name='generate_timetable'),
+    path('generate', views.generate, name='generate'),
+    path('generate_timetable', views.generate, name='generate_timetable'),
 ]
